2218615bcc221d68ebbc7c3e3fbd1ac7ca490c7b
sergiobelli81 <sergiobelli81@localhost>
2012-11-02 14:01:01 +0100

    gestione meccanismo di login

--- LoginManager.php
+++ LoginManager.php
@@ -12,6 +12,28 @@ class LoginManager {
 			"SELECT id FROM login WHERE username = '$username' AND password = '$password'");
     }
 
+    function fDateDiff($dateFrom, $dateA) {
+    
+    	if ($dateA != '') {
+    		list($d, $m, $y) = explode('/', $dateA);
+    		$mk=mktime(0, 0, 0, $m, $d, $y);
+    		$dateTo=strftime('%Y-%m-%d',$mk);
+    	} else {
+    		return 100;
+    	}
+    		
+    	if(empty($dateFrom)) $dateFrom = date('Y-m-d');
+    	if(empty($dateTo)) $dateTo = date('Y-m-d');
+    		
+    	$a_1 = explode('-',$dateFrom);
+    	$a_2 = explode('-',$dateTo);
+    	$mktime1 = mktime(0, 0, 0, $a_1[1], $a_1[2], $a_1[0]);
+    	$mktime2 = mktime(0, 0, 0, $a_2[1], $a_2[2], $a_2[0]);
+    	$secondi = $mktime1 - $mktime2;
+    	$giorni = intval($secondi / 86400);
+    	return -($giorni);
+    }
+    
 	function gestioneCertificatiMedici() {
 		$CertificatoMedicoManager = new CertificatoMedicoManager();
 		
@@ -154,4 +176,5 @@ class LoginManager {
 			}
 	
 	}
+	
 }
